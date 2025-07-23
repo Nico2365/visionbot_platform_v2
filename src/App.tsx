707fcc2c315ba11872147bbaa95b83274b18824f
Nico Gruendler <nico.gruendler@gmx.de>
2025-07-23 09:30:09 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>VisionBot Dashboard</h1>
+      <p>Willkommen zur Überwachungsplattform des Bots.</p>
+    </div>
+  );
+}
+
+export default App;
